8800dbcf05beac829a8b10477ee0c7944cf5e5cd
Sankalp Kulkarni <sn573012@dal.ca>
2023-04-19 03:34:53 -0300

    Added schema and Book GraphQL type

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express();
 const portNum = 8080;
-
+const {graphqlHTTP} = require("express-graphql")
+console.log(graphqlHTTP)
 app.listen(portNum, ()=>{
     console.log(`Server started on port ${portNum}`);
 });
+
+app.use("/graphql", graphqlHTTP({}));
