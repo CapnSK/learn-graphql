3cd8b8224bcccd049c3fadd77eb62909beededd1
Sankalp Kulkarni <sn573012@dal.ca>
2023-05-05 20:19:31 -0300

    externalized username and password for security

--- server/app.js
+++ server/app.js
@@ -1,22 +1,33 @@
 const express = require("express")
 const app = express();
 var cors = require('cors')
+const fs = require("fs");
 
 const portNum = 8080;
-const {graphqlHTTP} = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kulkarnisankalp21:SKsk%4012_@cluster0.h9todqn.mongodb.net/learn-graphql?retryWrites=true&w=majority");
+fs.readFile('./.config', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    const [rawUser, rawPwd] = data.split("\n");
+    const user = rawUser.split(":")[1];
+    const pwd = rawPwd.split(":")[1];
+    mongoose.connect(`mongodb+srv://${user}:${pwd}@cluster0.h9todqn.mongodb.net/learn-graphql?retryWrites=true&w=majority`);
+});
 
-mongoose.connection.once("open", ()=>{
+
+mongoose.connection.once("open", () => {
     console.log("Connected to DB");
 });
 
 
 console.log(graphqlHTTP)
 app.use(cors());
-app.listen(portNum, ()=>{
+app.listen(portNum, () => {
     console.log(`Server started on port ${portNum}`);
 });
 
